5cb8a324e7ece3047a43f6f69e0ba5a65a80467d
Dina <dinagiacobbi@gmail.com>
2022-09-05 09:43:32 -0800

    Adding Start Features

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,2 +1,11 @@
-const modal = document.getElementById('add-modal')
-modal.classList.add('visible')
+const addMovieModal = document.getElementById('add-modal')
+
+const startAddMovieButton = document.querySelector('header button')
+// const startAddMovieButton = document.querySelector('header').lastElementChild
+
+
+const toggleVisibleMovieModal = ()  => {
+    addMovieModal.classList.toggle('visible')
+}
+
+startAddMovieButton.addEventListener('click', toggleVisibleMovieModal)
