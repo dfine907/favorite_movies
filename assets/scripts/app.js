70f623c211254bef7e8215ab1d3962cf970949d5
Dina <dinagiacobbi@gmail.com>
2022-09-05 13:13:55 -0800

    Creating new movie array

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,14 +1,44 @@
 const addMovieModal = document.getElementById("add-modal")
-
 const startAddMovieButton = document.querySelector("header button")
-// const startAddMovieButton = document.querySelector('header').lastElementChild
-const backdrop = document.getElementById('backdrop')
+//alt const startAddMovieButton = document.querySelector('header').lastElementChild
+const backdrop = document.getElementById("backdrop")
 //alt const backdrop = document.body.firstElementChild
-const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive')
+const cancelAddMovieButton =
+  addMovieModal.querySelector(".btn--passive")
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling
+const userInputs = addMovieModal.querySelectorAll("input")
+const entryTextSection = document.getElementById("entry-text")
+
+const movies = []
+
+const updateUI = () => {
+  if (movies.length === 0) {
+    entryTextSection.style.display = "block"
+  } else {
+    entryTextSection.style.display = "none"
+  }
+}
+
+const renderNewMovieElement = (title, imageUrl, rating) => {
+  const newMovieElement = document.createElement('li')
+  newMovieElement.className = 'movie-element'
+  newMovieElement.innerHTML = `
+  <div class='movie-element__image>
+  <img src="${imageUrl}" alt="${title}>
+
+  </div>
+  <div class="movie-element__info">
+  <h2>${title}</h2>
+  <p>${rating}/5 Stars </p>
+  </div>
+  `
+  const listRoot = document.getElementById('movie-list')
+  listRoot.append(newMovieElement)
+}
 
 //button to switch the background class to gray
 const toggleBackDrop = () => {
-    backdrop.classList.toggle('visible')
+  backdrop.classList.toggle("visible")
 }
 
 //button with function to make modal visible
@@ -17,14 +47,49 @@ const toggleVisibleMovieModal = () => {
   toggleBackDrop()
 }
 
-const backdropClickHandler = () => {
-    toggleVisibleMovieModal()
- }
+const clearMovieInputs = () => {
+  userInputs.forEach((usrInput) => (usrInput.value = ""))
+}
 
- const cancelAddMovie = () => {
-    toggleVisibleMovieModal()
- }
+const backdropClickHandler = () => {
+  toggleVisibleMovieModal()
+}
+
+const cancelAddMovieHandler = () => {
+  toggleVisibleMovieModal()
+  clearMovieInputs()
+}
+
+const addMovieHandlder = () => {
+  const titleValue = userInputs[0].value
+  const imageUrlValue = userInputs[1].value
+  const ratingValue = userInputs[2].value
+
+  if (
+    titleValue.trim() === "" ||
+    imageUrlValue.trim() === "" ||
+    ratingValue.trim() === "" ||
+    +ratingValue < 1 ||
+    +ratingValue > 5
+  ) {
+    alert("Please enter valid values (rating between 1 and 5")
+    return
+  }
+  const newMovie = {
+    title: titleValue,
+    image: imageUrlValue,
+    rating: ratingValue,
+  }
+
+  movies.push(newMovie)
+  console.log(movies)
+  toggleVisibleMovieModal()
+  clearMovieInputs()
+  renderNewMovieElement(newMovie.title, newMovie.image, newMovie.rating)
+  updateUI()
+}
 
 startAddMovieButton.addEventListener("click", toggleVisibleMovieModal)
-backdrop.addEventListener('click',backdropClickHandler)
-cancelAddMovieButton.addEventListener('click', cancelAddMovie)
+backdrop.addEventListener("click", backdropClickHandler)
+cancelAddMovieButton.addEventListener("click", cancelAddMovieHandler)
+confirmAddMovieButton.addEventListener("click", addMovieHandlder)
